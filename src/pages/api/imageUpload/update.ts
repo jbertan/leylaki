12357c89bc0448edd34f62cb3098ae46d7db4f2d
jbertan <bertanakarsu@gmail.com>
2023-03-04 20:17:45 +0300

    delete function upgraded

--- src/pages/api/imageUpload/update.ts
+++ src/pages/api/imageUpload/update.ts
@@ -3,14 +3,14 @@ import { ObjectId } from "mongodb";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const Update = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { _id, kod, etiket, categories, picture } = req.body;
+  const { _id, kod, etiket, categories, picture, fileName } = req.body;
 
   const result = await productUpdate({
     kod,
     etiket,
     _id: new ObjectId(_id),
     categories,
-    picture,
+    picture: fileName,
   });
   console.log(picture, typeof _id);
   res.status(200).json({ message: "Marveleous" });
